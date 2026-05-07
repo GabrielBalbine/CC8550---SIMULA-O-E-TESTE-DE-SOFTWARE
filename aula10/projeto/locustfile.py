aacde338f29d9bc074b0cddd7bb129af33966ef4
Gabriel Balbine <infinitybraw@gmail.com>
2026-05-07 23:21:09 +0000

    lab 9 e 10 corrigidos

--- aula10/projeto/locustfile.py
+++ aula10/projeto/locustfile.py
@@ -1,18 +1,10 @@
 from locust import HttpUser, task, between
 
 class UsuarioBlackFriday(HttpUser):
+    host = "http://localhost:8000"
+
     wait_time = between(1, 3)
 
-    @task(3) 
-    def navegar_produtos(self):
-        pass
-
-    @task(1)
-    def finalizar_compra(self):
-        pass
-
-
-# Para Teste de Carga (> 2000 req/s):
-# Comando: locust -f locustfile.py --users 10000 --spawn-rate 100
-# Para Teste de Estresse (Achar ponto de quebra > 15.000 usuários):
-# Comando: locust -f locustfile.py --users 20000 --spawn-rate 500 
+    @task
+    def home(self):
+        self.client.get("/")
